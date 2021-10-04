a376841085d207efb0ec8889233904d1058477cf
necuteboy <89706104+necuteboy@users.noreply.github.com>
2021-10-04 18:36:35 +0500

    Update кодировка .js

--- кодировка .js
+++ кодировка .js
@@ -1,15 +1,15 @@
 let fs = require('fs');//переменная fs-работа с файловой системой
 let arg = process.argv;//объект для работы с элементами ком строки
 let inText;
-let stroka='abbccc';
+let stroka='abbbbbbbbbbbbbbbbbbbbbbbbbbbb';
 let k=0;
-function node(letter,freq,used,father,code,freq2){
+function node(letter,freq,used,father,code){
     this.letter=letter;
     this.freq=freq;
     this.used=used;
     this.father=father;
     this.code=code;
-    this.freq2=freq2;
+    //this.freq2=freq2;
 }
 let alph=new Array();
 let tree=new Array();
@@ -22,7 +22,7 @@ for (i=0;i<stroka.length;i++){
     alph[stroka.charAt(i)]++;
 }
 for (i in alph){
-    let n=new node(i,alph[i],false,null,'',alph[i]);
+    let n=new node(i,alph[i],false,null,'');
     tree.push(n)
 }
 let mina=0,premin=0,ind=0,maxim=0,ind3;
@@ -38,13 +38,14 @@ for (let j=0;j<tree.length;j++){
         premin=tree[j].freq;
         ind2=j
     }
+    /*
     if (maxim<tree[j].freq2){
         maxim=tree[j].freq2
         ind3=j;
-    }
+    }*/
 }
 // кодировка листьев в зависимости от их частоты
-tree[ind3].code='0';
+/*tree[ind3].code='0';
 tree[ind3].freq2=0;
 maxim=0;
 for (let i=0;i<tree.length-1;i++){
@@ -65,37 +66,69 @@ tree[ind3].code='';
 for (j=0;j<tree.length-1;j++){
     tree[ind3].code+='1';
 }
-
+*/
 // слияние двух минимумов
-n=new node(tree[ind].letter+tree[ind2].letter,tree[ind].freq+tree[ind2].freq,true,1,'');
-tree[ind].father=1;
-tree[ind].used=true;
-tree[ind2].used=true;
-tree[ind2].father=1;
-tree.push(n);
+if (premin!=0) {
+    n = new node(tree[ind2].letter + tree[ind].letter, tree[ind].freq + tree[ind2].freq, true, 1, '');
+    tree[ind].father = 1;
+    tree[ind].used = true;
+    tree[ind2].used = true;
+    tree[ind2].father = 1;
+    tree.push(n);
+
 //составление дерева
-for (i in alph){
-    let mina=0,ind=0;
-    for (let j=0;j<tree.length-k;j++){
-        if ((tree[j].freq<mina|| mina==0) && (tree[j].used==false)){
-            mina=tree[j].freq;
-            ind=j
+    for (i in alph) {
+        let mina = 0, ind = 0;
+        for (let j = 0; j < tree.length - k; j++) {
+            if ((tree[j].freq < mina || mina == 0) && (tree[j].used == false)) {
+                mina = tree[j].freq;
+                ind = j
+            }
+        }
+        if (mina == 0) {
+            break
+        }
+        n = new node(tree[ind].letter + tree[tree.length - 1].letter, tree[ind].freq + tree[tree.length - 1].freq, true, 1, '');
+        tree[ind].father = 1;
+        tree[ind].used = true;
+        tree.push(n);
+        k += 1;
+    }
+    tree[tree.length - 1].used = false;
+    tree[tree.length - 1].father = null;
+
+//кодировка листьев
+    for (i = 0; i < tree[tree.length - 1].letter.length; i++) {
+        for (let k = 0; k < tree.length - 1; k++) {
+            if (i < 1) {
+                if (tree[tree.length - 1].letter[i] == tree[k].letter) {
+                    tree[k].code += '0';
+                    console.log(tree[k].letter, 1, k);
+                    break
+                }
+            } else if (i >= 1) {
+                if (tree[tree.length - 1].letter[i] == tree[k].letter) {
+                    let t = 0;
+                    while (t < i) {
+                        tree[k].code += '1';
+                        t += 1;
+                    }
+                    if (i == tree[tree.length - 1].letter.length - 1) {
+                        break
+                    } else {
+                        tree[k].code += '0'
+                    }
+                    console.log(tree[k].letter, 2, k);
+                    break
+                }
+            }
         }
     }
-    if (mina==0){
-        break
-    }
-    n=new node(tree[ind].letter+tree[tree.length-1].letter,tree[ind].freq+tree[tree.length-1].freq,true,1,'');
-    console.log(mina,tree);
-    tree[ind].father=1;
-    tree[ind].used=true;
-    tree.push(n);
-    k+=1;
 }
-tree[tree.length-1].used=false;
-tree[tree.length-1].father=null;
-console.log(tree);
-//кодировка листьев
+else{
+    tree[0].code='0'
+}
+console.log(tree)
 
 
 
